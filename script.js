8d4a1788b5c06618d3f820bcff0e52b8aa73ce9d
gustavo <gustavo_klahold@estudante.sesisenai.org.br>
2025-03-20 10:09:02 -0300

    add

--- script.js
+++ script.js
@@ -1,9 +1,41 @@
-let verificarPar = prompt("digite um numero")
-
-console.log(verificarPar)
-
-if (verificarPar %2===0) {
-     console.log("o numero é par")
-} else {
-     console.log("o numero é impar")
+console.log("Hello, world!")
+let numero = parseInt(prompt("digite um numero: "));
+function verificarPar(numero){
+    if (numero % 2 === 0) {
+        console.log(numero + " é par");
+    } else {
+        console.log(numero + " é impar");
+    }
 }
+verificarPar(numero);
+
+let n1 = parseInt(prompt("digite um numero"));
+let n2 = parseInt(prompt("digite um numero"));
+function calcular(n1,n2) {
+
+    const soma = (n1+n2);
+    const sub = (n1-n2);
+    const mult = (n1*n2);
+    const div = (n1/n2);
+
+    console.log("o resultado da soma " + soma);
+    console.log("o resultado da subtração " + sub);
+    console.log("o resultado da multiplicação " + mult);
+    console.log("o resultado da divisão " + div);
+}
+calcular(n1,n2)
+
+let n = 11;
+let x = 0;
+
+while (n > 1) {
+  n--;
+  x -= n;
+  console.log(n);
+}
+
+let str = prompt("digite um texto?")
+function inverterTexto(str) {
+    
+}
+inverterTexto(str)
