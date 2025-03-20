09eb80e613a52bc96f4e3ad3b2185e9302ab8254
gustavo <gustavo_klahold@estudante.sesisenai.org.br>
2025-03-20 08:49:36 -0300

    add

---
+++ script.js
@@ -0,0 +1,9 @@
+let verificarPar = prompt("digite um numero")
+
+console.log(verificarPar)
+
+if (verificarPar %2===0) {
+     console.log("o numero é par")
+} else {
+     console.log("o numero é impar")
+}
